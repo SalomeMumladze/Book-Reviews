2079d842cde1e9bf0713001531cc423ea7a2a04a
salomeMumladze <salomemumladze775@gmail.com>
2025-09-12 14:13:15 +0400

    book controller

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+       $title = $request->input('title');
+    // if title is not null or not empty, it will run this function. Otherwise, it just won't.
+    // So if this first value passed to this first argument is not null, then this 
+    // function is called and this value is being passed.
+       $books = Book::when($title,
+       fn($query, $titel) => $query->title($title)
+       )->get();
+
+      return view('books.index', ['books' => $books]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
