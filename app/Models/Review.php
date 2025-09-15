d9970817214b5f7f2538028dbacf8543855f2b8e
salomeMumladze <salomemumladze775@gmail.com>
2025-09-15 16:23:31 +0400

    add create review blade

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Cache;
 
 class Review extends Model
 {
@@ -17,8 +16,10 @@ class Review extends Model
         return $this->belongsTo(Book::class);
     }
 
-protected static function booted(){
-    static::updated(fn(Review $review) => Cache::forget('book:'.$review->book_id));
-    static::deleted(fn(Review $review) => Cache::forget('book:'.$review->book_id));
-}
+    protected static function booted()
+    {
+        static::updated(fn(Review $review) => cache()->forget('book:' . $review->book_id));
+        static::deleted(fn(Review $review) => cache()->forget('book:' . $review->book_id));
+        static::created(fn(Review $review) => cache()->forget('book:' . $review->book_id));
+    }
 }
